22922591c260618400e50b3cbe41963378ef4d99
Seif Lotfy <seif.lotfy@gmail.com>
2021-01-07 12:59:21 +0100

    add proper path for handling

--- main.go
+++ main.go
@@ -10,6 +10,11 @@ import (
 	httpProxy "github.com/axiomhq/axiom-loki-proxy/http"
 )
 
+func initHttpPushHandler(mux *http.ServeMux, client *axiom.Client) {
+	handler := httpProxy.NewPushHandler(client)
+	mux.Handle("/loki/api/v1/push", handler)
+}
+
 func main() {
 	var (
 		deploymentURL = os.Getenv("AXM_DEPLOYMENT_URL")
@@ -23,8 +28,7 @@ func main() {
 	}
 
 	mux := http.NewServeMux()
-	handler := httpProxy.NewPushHandler(client)
-	mux.Handle("/", handler)
+	initHttpPushHandler(mux, client)
 
 	log.Printf("Now listening on %s...\n", *addr)
 	server := http.Server{Handler: mux, Addr: *addr}
