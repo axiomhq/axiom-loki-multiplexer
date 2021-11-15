50ed2f3a87030c7e6e45e296fc7e93825ede8091
Seif Lotfy <seif.lotfy@gmail.com>
2021-11-15 21:32:42 +0100

    fix multiplexing and refactor

--- http/push.go
+++ http/push.go
@@ -1,71 +1,146 @@
 package http
 
 import (
+	"bytes"
 	"context"
 	"fmt"
-	"log"
+	"io"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"reflect"
 	"sync"
 
 	"github.com/axiomhq/axiom-go/axiom"
-)
-
-const (
-	defaultDataset = "axiom-loki-proxy"
-	datasetKey     = "_axiom_dataset"
+	"go.uber.org/zap"
 )
 
 type ingestFunc func(ctx context.Context, id string, opts axiom.IngestOptions, events ...axiom.Event) (*axiom.IngestStatus, error)
 
-// PushHandler implements http.Handler.
-type PushHandler struct {
-	sync.Mutex
-	ingestFn ingestFunc
-}
+var logger *zap.Logger
 
-// NewPushHandler creates a new PushHandler which uses the passed Axiom client
-// to send logs to Axiom.
-func NewPushHandler(client *axiom.Client) *PushHandler {
-	return &PushHandler{
-		ingestFn: client.Datasets.IngestEvents,
+func init() {
+	var err error
+	logger, err = zap.NewProduction()
+	if err != nil {
+		panic(err)
+	}
+
+	// type check on axiom.Event incase it's ever not a map[string]interface{}
+	// so we can use unsafe.Pointer for a quick type conversion instead of allocating a new slice
+	if !reflect.TypeOf(axiom.Event{}).ConvertibleTo(reflect.TypeOf(map[string]interface{}{})) {
+		panic("axiom.Event is not a map[string]interface{}, please contact support")
 	}
 }
 
-// ServeHTTP implements http.Handler.
-func (push *PushHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	push.Lock()
-	defer push.Unlock()
+type lokiServer struct {
+	proxy *httputil.ReverseProxy
+	URL   *url.URL
+}
+
+func (lk *lokiServer) Host() string {
+	return lk.URL.Host
+}
+
+func (lk *lokiServer) Scheme() string {
+	return lk.URL.Scheme
+}
+
+// Multiplexer implements http.Handler.
+type Multiplexer struct {
+	sync.Mutex
+	defaultDataset string
+	datasetKey     string
+	lokiServer     *lokiServer
+	ingestFn       ingestFunc
+}
+
+// NewMultiplexer creates a new Multiplexer which uses the passed Axiom client
+// to send logs to Axiom.
+func NewMultiplexer(ingestFn ingestFunc, lokiEndpoint, defaultDataset, datasetKey string) (*Multiplexer, error) {
+	var lk *lokiServer
+	if lokiEndpoint != "" {
+		hcURL, err := url.Parse(lokiEndpoint)
+		if err != nil {
+			return nil, err
+		}
+		proxy := httputil.NewSingleHostReverseProxy(hcURL)
+		lk = &lokiServer{proxy: proxy, URL: hcURL}
+	}
+	return &Multiplexer{
+		ingestFn:       ingestFn,
+		lokiServer:     lk,
+		defaultDataset: defaultDataset,
+		datasetKey:     datasetKey,
+	}, nil
+}
+
+func (m *Multiplexer) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
+	if m.lokiServer != nil {
+		body := bytes.NewBuffer(nil)
+		req.Body = io.NopCloser(io.TeeReader(req.Body, body))
+		m.forward(resp, req)
+		req.Body = io.NopCloser(body)
+	}
+
+	if err := m.multiplex(req); err != nil {
+		logger.Error(err.Error())
+		if m.lokiServer == nil {
+			if _, wErr := resp.Write([]byte(err.Error())); wErr != nil {
+				logger.Error(wErr.Error())
+			}
+		}
+	}
+
+	if m.lokiServer == nil {
+		if _, wErr := resp.Write([]byte("{}")); wErr != nil {
+			logger.Error(wErr.Error())
+		}
+	}
+}
+
+func (m *Multiplexer) forward(resp http.ResponseWriter, req *http.Request) {
+	req.URL.Host = m.lokiServer.Host()
+	req.URL.Scheme = m.lokiServer.Scheme()
+	m.lokiServer.proxy.ServeHTTP(resp, req)
+}
+
+func (m *Multiplexer) multiplex(req *http.Request) error {
+	defer req.Body.Close()
+	if req.Method != "POST" {
+		return nil
+	}
 
 	var (
-		req *pushRequest
-		err error
+		ingestReq *pushRequest
+		err       error
 	)
 
-	typ := r.Header.Get("Content-Type")
+	typ := req.Header.Get("Content-Type")
 	switch typ {
 	case "application/json":
-		req, err = decodeJSONPushRequest(r.Body)
+		ingestReq, err = decodeJSONPushRequest(req.Body)
 	case "application/x-protobuf":
-		req, err = decodeProtoPushRequest(r.Body)
+		ingestReq, err = decodeProtoPushRequest(req.Body)
 	default:
 		err = fmt.Errorf("unsupported Content-Type %v", typ)
 	}
 
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		logger.Error(err.Error())
+		return err
 	}
 
 	var (
 		events  = make([]axiom.Event, 0)
-		dataset = defaultDataset
+		dataset = m.defaultDataset
 	)
 
-	for _, stream := range req.Streams {
+	for _, stream := range ingestReq.Streams {
 		labels := stream.Labels.Map()
-		if val, ok := labels[datasetKey]; ok {
+		if val, ok := labels[m.datasetKey]; ok {
 			dataset = val
-			delete(labels, datasetKey)
+			delete(labels, m.datasetKey)
 		}
 
 		for _, val := range stream.Entries {
@@ -78,10 +153,11 @@ func (push *PushHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			events = append(events, ev)
 		}
 
-		if _, err := push.ingestFn(context.Background(), dataset, axiom.IngestOptions{}, events...); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			log.Fatalln(err)
+		if _, err := m.ingestFn(context.Background(), dataset, axiom.IngestOptions{}, events...); err != nil {
+			logger.Error(err.Error())
+			return err
 		}
 		events = make([]axiom.Event, 0)
 	}
+	return nil
 }
